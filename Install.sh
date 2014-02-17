1a27b98607947c4a53fd687fac6830c6b7fb58b0
openproject <673592063@qq.com>
2014-02-18 00:04:24 +0800

    add custom java and xml snippets for android

--- Install.sh
+++ Install.sh
@@ -1,3 +1,7 @@
 cp .vimrc ~/.vimrc
 git submodule init
 git submodule update
+
+# copy custom snippets
+cp snippets/java.snippets bundle/snipmate/snippets/java.snippets
+cp snippets/xml.snippets bundle/snipmate/snippets/xml.snippets
